389052d31be14cd9d3cea59e446667bb804e31a4
Adam Davies <adam@thisismydesign.co.uk>
2015-02-27 18:07:23 +0000

    Revert df3f349..3aa4fd2
    This rolls back to commit df3f34927c49ef8cbcff0b5af3245a10987a362d.

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello Content Lemon Dropbox Edit number 2 <br/>";
-echo date('D M Y');
+<head>
+	<!-- General Meta --->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Facebook Meta --->
+	<meta property="og:image" content="img/avatar.png"/>
+	<meta property="og:title" content="Content Lemon - Content Collaboration"/>
+	<meta property="og:url" content="http://contentlemon.com"/>
+	<meta property="og:site_name" content="Content Lemon"/>
+	
+	<!-- Title -->
+	<title>Content Lemon - Web Content Made Easy</title>
+	
+	<!-- Favicon -->
+	<link rel="icon" href="favicon.ico">
+	
+	<!-- Stylesheets --->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css?v=1.9">
+	
+	
+</head>
+
+<body>
+
+<!-- Container --->
+<div class="container">
+	
+	<img class="center-block logo img-responsive" src="img/logo-white-web.png" />
+	
+	<h2 class="text-center">In Production</h2>
+	
+	
+</div>
+
+<!-- Scripts --->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
 
 
-?>
+</body>
+
+</html>
