1bd3ecc97e57e1661affcc0c341672936435a54e
Adam Davies <adam@thisismydesign.co.uk>
2015-03-02 13:29:18 +0000

    Test HTML Doc for Projects
    Test index for projects

---
+++ src/test/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- General Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Facebook Meta -->
+	<meta property="og:image" content="img/avatar.png"/>
+	<meta property="og:title" content="Content Lemon - Content Collaboration"/>
+	<meta property="og:url" content="http://contentlemon.com"/>
+	<meta property="og:site_name" content="Content Lemon"/>
+	
+	<!-- Title -->
+	<title>Content Lemon - Web Content Made Easy</title>
+	
+	<!-- Favicon -->
+	<link rel="icon" href="favicon.ico">
+	
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Maven+Pro:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/main.css?v=1.9">
+	
+	
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+          	<img src="./img/logo-white.png" alt="Content Lemon" />
+          </a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="#">Projects</a></li>
+            <li><a href="#about">Deadlines</a></li>
+            <li><a href="#contact">Help</a></li>
+            <!-- User Menu Option -->
+            <li>
+            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            		Adam Davies <span class="caret"></span> <img src="./img/user-avatar.png" class="img-circle" alt="Adam Davies"/>
+            	</a>
+            	<ul class="dropdown-menu" role="menu">
+		        	<li><a href="#"><i class="fa fa-fw fa-cog"></i> Settings</a></li>
+		            <li><a href="#"><i class="fa fa-fw fa-user"></i> Profile</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#"><i class="fa fa-fw fa-file"></i> Create Project</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#"><i class="fa fa-fw fa-power-off"></i> Logout</a></a></li>
+		        </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+<!-- ./ Navigation End -->
+
+<!-- Page Content Start -->
+<div class="page-content">
+
+	<!-- container -->
+	<div class="container">
+
+		<!-- Title row -->
+		<div class="row title-row">
+
+			<!-- Main Column -->
+			<div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
+				<header class="section-heading">
+					<h1 class="page-heading">
+						Projects<span class="badge badge-info">12</span>
+					</h1>
+				</header>
+			</div>
+			<!-- ./main col end -->
+
+			<!-- Action Column -->
+			<div class="col-lg-4 col-md-4 col-sm-5 hidden-xs">
+				<header class="section-heading">
+					<h1 class="page-heading">
+						Status
+					</h1>
+				</header>
+			</div>
+
+		</div>
+		<!-- ./row title row -->
+
+
+	</div>
+	<!-- ./container -->
+
+
+
+</div>
+<!-- ./page-content -->
+
+
+<!-- Scripts -->
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+
+
+</body>
+
+</html>
